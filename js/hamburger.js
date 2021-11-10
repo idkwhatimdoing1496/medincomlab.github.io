82568e719a47f48a9e6f284b5f4afb77a9012acf
Dmitro.savenko <90914753+medincomlab@users.noreply.github.com>
2021-11-10 22:05:42 +0300

    Add files via upload

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,22 +1,18 @@
-const menu = document.getElementById("header");
-const menuItems = document.getElementsByClassName(".navButton");
-const hamburger= document.querySelector(".hamburgerMenu");
-const navIn= document.querySelector(".navIn");
+const menu = document.querySelector(".navIn");
+const menuItems = document.querySelectorAll(".navButton");
+const hamburger= document.querySelector(".hamburger");
 const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".openIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
         menu.classList.remove("showMenu");
         closeIcon.style.display = "none";
         menuIcon.style.display = "block";
-        navIn.style.transform = "none";
     } else {
         menu.classList.add("showMenu");
         closeIcon.style.display = "block";
         menuIcon.style.display = "none";
-        navIn.style.transform = "translateY(100%)";
     }
 }
-
 hamburger.addEventListener("click", toggleMenu);
